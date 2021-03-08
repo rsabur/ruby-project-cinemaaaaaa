ad1af20625379f4825e28d8195a06431a2620d12
Radiyah <rsabur58@gmail.com>
2021-03-08 18:18:08 -0500

    environment setup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+gem "activerecord", "~> 5.2"
 gem "sinatra-activerecord"
 gem "sqlite3"
 gem "pry"
 gem "require_all"
+gem "rake"
+gem 'tty-prompt'
+gem 'activerecord-reset-pk-sequence'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+# require 'bundler/setup'
+# Bundler.require
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+# require_all 'app'
