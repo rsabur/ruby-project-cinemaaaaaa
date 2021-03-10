f795a347b570210a8fbd42d43d8d865856ab189e
Susana <susana.m.vik@gmail.com>
2021-03-10 13:51:34 -0500

    fixed favorite list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,14 @@
 class Movie < ActiveRecord::Base
     has_many :favorites
     has_many :users, through: :favorites
+
+    def favorites
+        Favorite.all.select {|fav_movies| fav_movies.movie == self}
+    end
+
+    def users
+        self.favorites.map do |fav_list|
+            fav_list.user
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_many :favorites
     has_many :movies, through: :favorites 
 
+    attr_reader :name, :username, :password
+
     def self.login_helper_method
         puts "What is your username?"
         username = STDIN.gets.chomp
@@ -46,16 +48,32 @@ class User < ActiveRecord::Base
     def display_favorite_movies
         system 'clear'
         sleep 1
-        
+        if favorites.length > 0
+            array_of_hashes = self.movies.map do |movie|
+                binding.pry
+                movie.title
+            end
+            movie_id = TTY::Prompt.new.select("Which movie would you like more information on?", array_of_hashes)
+            puts "You chose #{movie.id}!"
+        else
         puts "You don't have any movies listed"
+        end
     end
 
     def create_new_favs_list
         system 'clear'
         sleep 1
         puts "Let's get started!"
-
     end
 
+    def favorites
+        Favorite.all.select {|fav_movies| fav_movies.user == self}
+    end
+    
+    def movies
+        self.favorites.map do |fav_list| 
+            fav_list.movie
+        end
+    end
     
 end
